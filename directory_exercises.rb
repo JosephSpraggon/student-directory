2f837602b75759ae981508b31aa447ce1c0c37c5
Joe Spraggon <joe_spraggon@hotmail.co.uk>
2020-12-20 14:14:40 +0000

    Added working interactive menu

--- directory_exercises.rb
+++ directory_exercises.rb
@@ -1,18 +1,24 @@
-=begin
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november}
-]
-=end
+def interactive_menu
+  students = []
+  loop do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+    selection = gets.chomp
+    case selection
+    when "1"
+      students = input_students("no name", "no age", "no cohort")
+    when "2"
+      print_header(students)
+      print_by_cohort(students)
+      print_footer(students)
+    when "9"
+      exit
+    else
+      puts "I don't know what you mean, try again"
+    end
+  end
+end
 
 def input_students(default_name, default_age, default_cohort)
   students = []
@@ -101,7 +107,4 @@ def print_footer(students)
   end
 end
 
-students = input_students("no name", "no age", "no cohort")
-print_header(students)
-print_by_cohort(students)
-print_footer(students)
+interactive_menu
